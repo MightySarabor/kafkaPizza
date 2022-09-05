315dfafa8768e939042939319a9dddeaf53597cc
MightySarabor <fleschm@hochschule-trier.de>
2022-09-05 22:37:52 +0200

    import missing class

--- streams.examples/src/main/java/myapps/Util/JsonTimestampExtractor.java
+++ streams.examples/src/main/java/myapps/Util/JsonTimestampExtractor.java
@@ -1,6 +1,7 @@
 package myapps.Util;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import myapps.PageViewTypedDemo;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.streams.processor.TimestampExtractor;
 
