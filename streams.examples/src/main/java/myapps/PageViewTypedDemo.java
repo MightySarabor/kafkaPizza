3978ba6de14540393a9e2e5ab5ac05d5a288c4da
MightySarabor <fleschm@hochschule-trier.de>
2022-09-06 19:03:40 +0200

    Added some testCaes

--- streams.examples/src/main/java/myapps/PageViewTypedDemo.java
+++ streams.examples/src/main/java/myapps/PageViewTypedDemo.java
@@ -19,7 +19,11 @@ package myapps;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import myapps.Util.Json.Json;
+import myapps.Util.Json.JsonPOJOSerializer;
 import myapps.Util.Json.JsonTimestampExtractor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.errors.SerializationException;
@@ -27,16 +31,14 @@ import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.Serializer;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.TimeWindows;
+
 
 import java.io.IOException;
 import java.time.Duration;
@@ -174,70 +176,35 @@ public class PageViewTypedDemo {
         public String region;
     }
 
-    public static void main(final String[] args) {
-        final Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pageview-typed");
+    public static void main(final String[] args) throws IOException {
+        //Test String
+        String test = "{\"_t\":\"pv\",\n" +
+                "  \"user\":\"hans\",\n" +
+                "  \"page\":\"news\",\n" +
+                "  \"timestamp\":12908734094\n" +
+                "}";
+        PageView pageView = Json.fromJson(Json.parse(test), PageView.class);
+        JsonPOJOSerializer<PageView> jp= new JsonPOJOSerializer<>();
+        System.out.println(jp.serialize("test",  pageView));
+
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test_stream");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, JsonTimestampExtractor.class);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, JSONSerde.class);
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JSONSerde.class);
-        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1000L);
 
-        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        System.out.println("Starting stream...");
         final StreamsBuilder builder = new StreamsBuilder();
+
         builder.stream("my_first",
                         Consumed.with(Serdes.String(), new JSONSerde<>()))
-                .peek((k, pv) -> System.out.println("test " + pv));
-        //final KStream<String, PageView> views = builder.stream("my_first", Consumed.with(Serdes.String(), new JSONSerde<>()));
+                .peek((k, pv) -> System.out.println(pv));
 
-        // KTable<String, UserProfile> users = builder.table("streams-userprofile-input", Consumed.with(Serdes.String(), new JSONSerde<>()));
-
-        //final Duration duration24Hours = Duration.ofHours(24);
-
-
-
-        /*final KStream<WindowedPageViewByRegion, RegionCount> regionCount = views
-                .leftJoin(users, (view, profile) -> {
-                    final PageViewByRegion viewByRegion = new PageViewByRegion();
-                    viewByRegion.user = view.user;
-                    viewByRegion.page = view.page;
-
-                    if (profile != null) {
-                        viewByRegion.region = profile.region;
-                    } else {
-                        viewByRegion.region = "UNKNOWN";
-                    }
-                    return viewByRegion;
-                })
-                .map((user, viewRegion) -> new KeyValue<>(viewRegion.region, viewRegion))
-                .groupByKey(Grouped.with(Serdes.String(), new JSONSerde<>()))
-                .windowedBy(TimeWindows.ofSizeAndGrace(Duration.ofDays(7), duration24Hours).advanceBy(Duration.ofSeconds(1)))
-                .count()
-                .toStream()
-                .map((key, value) -> {
-                    final WindowedPageViewByRegion wViewByRegion = new WindowedPageViewByRegion();
-                    wViewByRegion.windowStart = key.window().start();
-                    wViewByRegion.region = key.key();
-
-                    final RegionCount rCount = new RegionCount();
-                    rCount.region = key.key();
-                    rCount.count = value;
-
-                    return new KeyValue<>(wViewByRegion, rCount);
-                });
-
-        // write to the result topic
-        regionCount.to("streams-pageviewstats-typed-output", Produced.with(new JSONSerde<>(), new JSONSerde<>()));
-        */
-
-        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        final Topology topology = builder.build();
+        final KafkaStreams streams = new KafkaStreams(topology, props);
         final CountDownLatch latch = new CountDownLatch(1);
 
         // attach shutdown handler to catch control-c
-        Runtime.getRuntime().addShutdownHook(new Thread("streams-pipe-shutdown-hook") {
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
             @Override
             public void run() {
                 streams.close();
@@ -248,8 +215,7 @@ public class PageViewTypedDemo {
         try {
             streams.start();
             latch.await();
-        } catch (final Throwable e) {
-            e.printStackTrace();
+        } catch (Throwable e) {
             System.exit(1);
         }
         System.exit(0);
