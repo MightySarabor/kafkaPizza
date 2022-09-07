d75e2b7ab860449dc76076397febbb32fb5e089a
MightySarabor <fleschm@hochschule-trier.de>
2022-09-07 15:39:15 +0200

    simplified

--- streams.examples/src/main/java/myapps/PizzaOrders.java
+++ streams.examples/src/main/java/myapps/PizzaOrders.java
@@ -153,43 +153,9 @@ public class PizzaOrders {
     }
 
     public class PizzaPOJO implements JSONSerdeCompatible {
-        private String name;
-        private String size;
-        private float price;
-
-        public PizzaPOJO() {
-
-        }
-
-        public PizzaPOJO(String name, String size, float price){
-            this.name = name;
-            this.size = size;
-            this.price = price;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getSize() {
-            return size;
-        }
-
-        public void setSize(String size) {
-            this.size = size;
-        }
-
-        public float getPrice() {
-            return price;
-        }
-
-        public void setPrice(float price) {
-            this.price = price;
-        }
+        public String name;
+        public String size;
+        public float price;
 
         @Override
         public String toString() {
