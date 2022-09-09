4d7d0b163217874fa20976d672912b8d6387ae8e
MightySarabor <fleschm@hochschule-trier.de>
2022-09-09 08:59:46 +0200

    changed producer

--- streams.examples/src/main/java/myapps/Util/Producer.java
+++ streams.examples/src/main/java/myapps/Util/Producer.java
@@ -33,7 +33,7 @@ public class Producer {
 
 
         while(true) {
-            String topic = "fleschm-one";
+            String topic = "fleschm-two";
             String value = Json.stringify(Json.toJson(generatePizza()));
             ProducerRecord<String, String> record =
                     new ProducerRecord<String, String>(topic, value);
