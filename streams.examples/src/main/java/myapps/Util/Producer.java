51b90d5a0856808a5ba5dcfb140fddbf2cbd7242
MightySarabor <fleschm@hochschule-trier.de>
2022-09-11 16:29:04 +0200

    deleted metadata

--- streams.examples/src/main/java/myapps/Util/Producer.java
+++ streams.examples/src/main/java/myapps/Util/Producer.java
@@ -18,7 +18,7 @@ public class Producer {
 
         //properties
         String bootstrapServers =
-                "infbdt07.fh-trier.de:6667";
+                "infbdt07.fh-trier.de:6667,infbdt08.fh-trier.de:6667,infbdt09.fh-trier.de:6667";
         Properties props = new Properties();
         props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -37,23 +37,10 @@ public class Producer {
             String value = Json.stringify(Json.toJson(generatePizza()));
             ProducerRecord<String, String> record =
                     new ProducerRecord<String, String>(topic, value);
-            System.out.println(value);
             //Sending data
             Thread.sleep(10000);
-            first_producer.send(record, new Callback() {
-                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-
-                    if (e == null) {
-                        System.out.println("Successfully recieved the details as: \n" +
-                                "Topic: " + recordMetadata.topic() + "\n" +
-                                "Partition: " + recordMetadata.partition() + "\n" +
-                                "Offset: " + recordMetadata.offset() + "\n" +
-                                "TimeStamp:" + recordMetadata.timestamp());
-                    } else {
-                        System.err.println("Can't produce, getting Error: " + e);
-                    }
-                }
-            }).get();
+            first_producer.send(record);
+            System.out.println("Message sent: " + value);
         }
     }
 }
